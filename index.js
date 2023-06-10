3f421d1ea4c7075b978159ab85ad058d25c0abf3
Khanh Nguyen <khanhcoluy@gmail.com>
2023-06-10 11:44:46 +0700

    send email function via EmailJs

--- index.js
+++ index.js
@@ -14,4 +14,37 @@ function PageTransition() {
   });
 }
 
+// send email via contact form
+function sendEmail(e) {
+  e.preventDefault();
+  emailjs.init('cTy18X7QbqztVR9aS');
+  // Fetch the user's input from the form
+
+  // Set EmailJS service ID, template ID, and user ID
+  const serviceID = 'service_orweqzm';
+  const templateID = 'template_sczuxy8';
+
+  // Get the form values
+  var templateParams = {
+    from_name: document.getElementById('name').value,
+    from_email: document.getElementById('email').value,
+    subject: document.getElementById('subject').value,
+    message: document.getElementById('message').value,
+  };
+
+  // Send the email using EmailJS
+  emailjs
+    .send(serviceID, templateID, templateParams)
+    .then(() => {
+      // Reset the form fields
+      document.getElementById('contact-form').reset();
+      alert('Your message has been sent successfully!');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert(
+        'An error occurred while sending the message. Please try again later.'
+      );
+    });
+}
 PageTransition();
